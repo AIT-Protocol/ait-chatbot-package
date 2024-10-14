d4dd8cf6cd40061cbe0636b9e6ae06a738b69eaf
khloeflechzo <khloe.flechazo@gmail.com>
2024-10-14 17:20:01 +0700

    fix: UI

--- src/Chatbot.tsx
+++ src/Chatbot.tsx
@@ -18,9 +18,20 @@ export const Chatbot = (): ReactElement => {
       </button>
       <div className={classNames('chat_box', { ['toggle']: toggleChatBot })}>
         <div className={'chat_box_header'}>
-          <TypoLabel size={14} className="leading-[150%]" color='gray'>
-            powered by AIT
-          </TypoLabel>
+          <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '8px'
+          }}>
+            <img src="https://app.aitprotocol.ai/icons/logo-ait-node.svg" alt="logo" width="28px" height="28px" style={{
+              width: '28px',
+              height: '28px',
+              borderRadius: '4px',
+            }} />
+            <TypoLabel size={14} className="leading-[150%]" color='gray'>
+              Powered by AIT
+            </TypoLabel>
+          </div>
           <button className={'chat_box_header_close'} onClick={() => setToggleChatBot(false)}>
             <img src="https://app.aitprotocol.ai/icons/arrow-down.svg" alt="close-icon" width="24" height="24" />
           </button>
